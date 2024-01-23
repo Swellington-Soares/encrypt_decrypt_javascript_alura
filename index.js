d04f0555bb6082a263bbb36f2aef9c6c44aa0828
Swellington Soares <swellington.santos@gmail.com>
2024-01-22 23:22:33 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const textInput = document.getElementById('text')
+const keyInput = document.getElementById('key')
+const resultTarget = document.getElementById('result')
+
+function parseText(value, key){
+    const tt = []
+
+    for (let i = 0; i<value.length; i++) {
+        tt.push( value.charCodeAt( i ) ^ key )
+    }
+
+    return tt.map( a => String.fromCharCode( a ) ).join("");
+}
+
+function codedecode() {
+    const value = textInput.value
+    const key = keyInput.value
+    resultTarget.value = '';
+
+    if (value && key) {
+        resultTarget.value = parseText(value, parseInt( key ))
+    }  else {
+        resultTarget.value = 'ERRO'
+    }
+}
