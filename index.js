b5a37d6db78877598e268cb78d1130a228d993a6
Swellington Soares <swellington.santos@gmail.com>
2024-01-23 22:06:19 -0300

    desafio alura concluído

--- index.js
+++ index.js
@@ -1,25 +1,90 @@
-const textInput = document.getElementById('text')
-const keyInput = document.getElementById('key')
-const resultTarget = document.getElementById('result')
+const keys = {
+  a: "ai",
+  e: "enter",
+  i: "imes",
+  o: "ober",
+  u: "ufat",
+};
 
-function parseText(value, key){
-    const tt = []
+const ViewModel = {
+  inputTextArea: document.getElementById("inputTextArea"),
+  btnEncode: document.getElementById("btnEncode"),
+  btnDecode: document.getElementById("btnDecode"),
+  containerResult: document.getElementById("containerResult"),
+  btnCopy: document.getElementById("btnCopy"),
+  resultTextArea: document.getElementById("resultTextArea"),
+  noCopyContainer: document.getElementById("noCopyContainer"),
+  errorMessage: document.getElementById("errorMessage"),
 
-    for (let i = 0; i<value.length; i++) {
-        tt.push( value.charCodeAt( i ) ^ key )
-    }
+  encryptedText: "",
+};
 
-    return tt.map( a => String.fromCharCode( a ) ).join("");
+function codificar(text) {
+  return Array.from(text.toLocaleLowerCase())
+    .map((x) => keys[x] ?? x)
+    .join("");
 }
 
-function codedecode() {
-    const value = textInput.value
-    const key = keyInput.value
-    resultTarget.value = '';
-
-    if (value && key) {
-        resultTarget.value = parseText(value, parseInt( key ))
-    }  else {
-        resultTarget.value = 'ERRO'
-    }
+function decodificar(text) {
+  const reverseKey = Object.fromEntries(
+    Object.entries(keys).map(([k, v]) => [v, k])
+  );
+  Object.keys(reverseKey).forEach((k) => {
+    text = text.split(k).join(reverseKey[k]);
+  });
+  return text;
 }
+
+function IsInputTextValid() {
+  return ViewModel.encryptedText.match(/^[a-z\s]+$/i);
+}
+
+function ShowError() {
+  ViewModel.errorMessage.style.color = "red";
+  setTimeout(() => {
+    ViewModel.errorMessage.style.color = "#343a40";
+  }, 500);
+}
+
+function ShowEmptyTextContainer(){
+    ViewModel.containerResult.style.display = 'none';
+    ViewModel.noCopyContainer.style.display = 'flex';
+}
+
+function ShowResultContainer(text){
+    ViewModel.resultTextArea.value = text
+    ViewModel.noCopyContainer.style.display = 'none';
+    ViewModel.containerResult.style.display = 'flex';
+}
+
+ViewModel.btnCopy.onclick = () => {
+    navigator.clipboard.writeText( ViewModel.encryptedText ).then(() => {
+        alert('Texto copiado para a área de transferência.')
+    });
+
+}
+
+ViewModel.btnEncode.onclick = () => {
+  ViewModel.encryptedText = ViewModel.inputTextArea.value;
+  if (!IsInputTextValid()) {
+    ShowEmptyTextContainer()
+    return ShowError();
+  }
+  ViewModel.encryptedText = codificar(ViewModel.encryptedText);
+  ViewModel.inputTextArea.value = ""
+  ShowResultContainer(ViewModel.encryptedText)
+};
+
+ViewModel.btnDecode.onclick = () => {
+  ViewModel.encryptedText = ViewModel.inputTextArea.value;
+
+  console.log(ViewModel.encryptedText)
+
+  if (!IsInputTextValid()) {
+    ShowEmptyTextContainer()
+    return ShowError();
+  }
+  ViewModel.encryptedText = decodificar(ViewModel.encryptedText);
+  ViewModel.inputTextArea.value = ""
+  ShowResultContainer(ViewModel.encryptedText)
+};
